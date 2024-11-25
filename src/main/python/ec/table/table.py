aaf9e4fc693addd80fc45bf28729a6d40587bf27
pareronia <49491686+pareronia@users.noreply.github.com>
2024-11-25 22:56:37 +0100

    implementation table module - add stats

--- src/main/python/ec/table/table.py
+++ src/main/python/ec/table/table.py
@@ -1,9 +1,30 @@
 import os
 
+from ..stats.stats import Part
+from ..stats.stats import PartStats
+from ..stats.stats import get_user_stats
+
 
 def get_py(year: int, day: int) -> str:
     path = os.path.join("src", "main", "python", f"{year}_{day:02}.py")
     return f"[✓]({path})" if os.path.exists(path) else ""
+
+
+def get_rank(stats: dict[Part, PartStats], year: int, day: int) -> str:
+    return "&nbsp;/&nbsp;".join(
+        str(stats[(year, day, p)].place).rjust(4).replace(" ", "&nbsp;")
+        for p in range(1, 4)
+    )
+
+
+def get_points(stats: dict[Part, PartStats], year: int, day: int) -> str:
+    scores = []
+    for p in range(1, 4):
+        s = stats[(year, day, p)].score
+        scores.append("-" if s == 0 else str(s))
+    return "&nbsp;/&nbsp;".join(
+        scores[p].rjust(3).replace(" ", "&nbsp;") for p in range(3)
+    )
 
 
 def main(file_name: str) -> None:
@@ -14,15 +35,21 @@ def main(file_name: str) -> None:
         in_table = False
         for line in tmp.splitlines():
             if line.startswith("<!-- @BEGIN:Quests"):
-                print(line, file=f)
-                print("| Quest | python3 | ", file=f)
-                print("| --- | --- |", file=f)
-                for i in range(1, 21):
-                    print(
-                        f"|[{i}]({url}{i})|{get_py(2024, i)}|",
-                        file=f,
-                    )
                 in_table = True
+                print(line, file=f)
+                year = int(line.split("@")[1].split(":")[2])
+                stats = get_user_stats(year)
+                print("| Quest | python3 | Rank | Points |", file=f)
+                print("| --- | --- | --- | --- |", file=f)
+                for day in range(1, 21):
+                    if (year, day, 3) in stats:
+                        py = get_py(year, day)
+                        rank = get_rank(stats, year, day)
+                        points = get_points(stats, year, day)
+                    else:
+                        py, rank, points = "", "", ""
+                    line = f"|[{day}]({url}{day})|{py}|{rank}|{points}|"
+                    print(line, file=f)
             elif line.startswith("<!-- @END:Quests"):
                 in_table = False
                 print(line, file=f)
