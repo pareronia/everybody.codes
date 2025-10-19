80f3631d9765e554fc5714ad579da0f4b5aeedf3
pareronia <49491686+pareronia@users.noreply.github.com>
2025-10-19 22:34:24 +0200

    Story 1 Quest 2 - cleanup

--- src/main/python/S01_02.py
+++ src/main/python/S01_02.py
@@ -5,12 +5,14 @@
 
 import sys
 from dataclasses import dataclass
+from enum import Enum
+from enum import auto
+from enum import unique
 from typing import Self
 
 from ec.common import InputData
 from ec.common import SolutionBase
 from ec.common import ec_samples
-from ec.common import log
 
 TEST1 = """\
 ADD id=1 left=[10,A] right=[30,H]
@@ -66,12 +68,8 @@ ADD id=6 left=[20,G] right=[32,K]
 ADD id=7 left=[4,E] right=[21,N]
 SWAP 2
 """
-TEST5 = (
-    TEST4
-    + """\
-SWAP 5
-"""
-)
+
+L, R = "L", "R"
 
 
 @dataclass
@@ -80,9 +78,6 @@ class Node:
     symbol: str
     left: Self | None = None
     right: Self | None = None
-
-    def __hash__(self) -> int:
-        return self.rank
 
     @classmethod
     def from_input(cls, string: str) -> Self:
@@ -99,30 +94,23 @@ class Node:
             node.right = cls.insert(node.right, other)
         return node
 
-    @classmethod
-    def by_level(
-        cls, root: Self | None, level: int, d: dict[int, set[Self]]
-    ) -> None:
-        if root is None:
-            return
-        d.setdefault(level, set[Self]()).add(root)
-        cls.by_level(root.left, level + 1, d)
-        cls.by_level(root.right, level + 1, d)
+    def swap_data(self, other: Self) -> None:
+        self.rank, other.rank = other.rank, self.rank
+        self.symbol, other.symbol = other.symbol, self.symbol
 
-    @classmethod
-    def ans(
-        cls, root: Self | None, level: int, levels: dict[int, set[Self]]
-    ) -> str:
-        if root is None:
-            return ""
-        root_level = next(k for k, v in levels.items() if root in v)
-        return (
-            root.symbol
-            if root_level == level
-            else ""
-            + cls.ans(root.right, level, levels)
-            + cls.ans(root.left, level, levels)
-        )
+    def swap_children(self, other: Self) -> None:
+        self.left, other.left = other.left, self.left
+        self.right, other.right = other.right, self.right
+
+    def __hash__(self) -> int:
+        return self.rank
+
+
+@unique
+class SwapMode(Enum):
+    NONE = auto()
+    NODE = auto()
+    SUBTREE = auto()
 
 
 Output1 = str
@@ -131,115 +119,59 @@ Output3 = str
 
 
 class Solution(SolutionBase[Output1, Output2, Output3]):
+    def solve(self, input_data: InputData, swap_mode: SwapMode) -> str:  # noqa:C901
+        def build_tree() -> None:
+            for line in input_data:
+                if line.startswith("ADD"):
+                    nid, left, right = (
+                        s.split("=")[1] for s in line.split()[1:]
+                    )
+                    for s, l_r in zip((left, right), (L, R), strict=True):
+                        node = Node.from_input(s)
+                        nids[l_r][nid] = node
+                        Node.insert(nids[l_r].get("1", None), node)
+                else:
+                    nid = line.split()[1]
+                    if swap_mode != SwapMode.NONE:
+                        nids[L][nid].swap_data(nids[R][nid])
+                    if swap_mode == SwapMode.SUBTREE:
+                        nids[L][nid].swap_children(nids[R][nid])
+
+        def ans(nids: dict[str, Node]) -> str:
+            def group_by_level(root: Node | None, level: int) -> None:
+                if root is None:
+                    return
+                by_level.setdefault(level, set[Node]()).add(root)
+                group_by_level(root.left, level + 1)
+                group_by_level(root.right, level + 1)
+
+            def get_symbols(root: Node | None, level: int) -> str:
+                if root is None:
+                    return ""
+                root_level = next(k for k, v in by_level.items() if root in v)
+                return (
+                    (root.symbol if root_level == level else "")
+                    + get_symbols(root.right, level)
+                    + get_symbols(root.left, level)
+                )
+
+            by_level = dict[int, set[Node]]()
+            group_by_level(nids["1"], 0)
+            level = max(by_level.keys(), key=lambda k: len(by_level[k]))
+            return get_symbols(nids["1"], level)
+
+        nids = {L: dict[str, Node](), R: dict[str, Node]()}
+        build_tree()
+        return ans(nids[L]) + ans(nids[R])
+
     def part_1(self, input_data: InputData) -> Output1:
-        root_left, root_right = None, None
-        for line in input_data:
-            _, nid, left, right = line.split()
-            left, right = left.split("=")[1], right.split("=")[1]
-            if nid.split("=")[1] == "1":
-                root_left = Node.insert(root_left, Node.from_input(left))
-                root_right = Node.insert(root_right, Node.from_input(right))
-            else:
-                Node.insert(root_left, Node.from_input(left))
-                Node.insert(root_right, Node.from_input(right))
-        left_d, right_d = dict[int, set[Node]](), dict[int, set[Node]]()
-        Node.by_level(root_left, 0, left_d)
-        Node.by_level(root_right, 0, right_d)
-        left_m = max(left_d.items(), key=lambda item: len(item[1]))[0]
-        right_m = max(right_d.items(), key=lambda item: len(item[1]))[0]
-        return Node.ans(root_left, left_m, left_d) + Node.ans(
-            root_right, right_m, right_d
-        )
+        return self.solve(input_data, SwapMode.NONE)
 
     def part_2(self, input_data: InputData) -> Output2:
-        root_left, root_right = None, None
-        nids_left, nids_right = dict[str, Node](), dict[str, Node]()
-        for line in input_data:
-            if line.startswith("ADD"):
-                _, nid, left, right = line.split()
-                nid = nid.split("=")[1]
-                left, right = left.split("=")[1], right.split("=")[1]
-                if nid == "1":
-                    root_left = Node.insert(root_left, Node.from_input(left))
-                    nids_left[nid] = root_left
-                    root_right = Node.insert(
-                        root_right, Node.from_input(right)
-                    )
-                    nids_right[nid] = root_right
-                else:
-                    node = Node.from_input(left)
-                    nids_left[nid] = node
-                    Node.insert(root_left, node)
-                    node = Node.from_input(right)
-                    nids_right[nid] = node
-                    Node.insert(root_right, node)
-            else:
-                _, nid = line.split()
-                swap_left, swap_right = nids_left[nid], nids_right[nid]
-                tmp_rank, tmp_symbol = swap_right.rank, swap_right.symbol
-                swap_right.rank = swap_left.rank
-                swap_right.symbol = swap_left.symbol
-                swap_left.rank = tmp_rank
-                swap_left.symbol = tmp_symbol
-        left_d, right_d = dict[int, set[Node]](), dict[int, set[Node]]()
-        Node.by_level(root_left, 0, left_d)
-        Node.by_level(root_right, 0, right_d)
-        left_m = max(left_d.items(), key=lambda item: len(item[1]))[0]
-        right_m = max(right_d.items(), key=lambda item: len(item[1]))[0]
-        return Node.ans(root_left, left_m, left_d) + Node.ans(
-            root_right, right_m, right_d
-        )
+        return self.solve(input_data, SwapMode.NODE)
 
     def part_3(self, input_data: InputData) -> Output3:
-        root_left, root_right = None, None
-        nids_left, nids_right = dict[str, Node](), dict[str, Node]()
-        for line in input_data:
-            if line.startswith("ADD"):
-                _, nid, left, right = line.split()
-                nid = nid.split("=")[1]
-                left, right = left.split("=")[1], right.split("=")[1]
-                if nid == "1":
-                    root_left = Node.insert(root_left, Node.from_input(left))
-                    nids_left[nid] = root_left
-                    root_right = Node.insert(
-                        root_right, Node.from_input(right)
-                    )
-                    nids_right[nid] = root_right
-                else:
-                    node = Node.from_input(left)
-                    nids_left[nid] = node
-                    Node.insert(root_left, node)
-                    node = Node.from_input(right)
-                    nids_right[nid] = node
-                    Node.insert(root_right, node)
-            else:
-                _, nid = line.split()
-                swap_left, swap_right = nids_left[nid], nids_right[nid]
-                swap_left.rank, swap_right.rank = (
-                    swap_right.rank,
-                    swap_left.rank,
-                )
-                swap_left.symbol, swap_right.symbol = (
-                    swap_right.symbol,
-                    swap_left.symbol,
-                )
-                swap_left.left, swap_right.left = (
-                    swap_right.left,
-                    swap_left.left,
-                )
-                swap_left.right, swap_right.right = (
-                    swap_right.right,
-                    swap_left.right,
-                )
-        log((root_left, root_right))
-        left_d, right_d = dict[int, set[Node]](), dict[int, set[Node]]()
-        Node.by_level(root_left, 0, left_d)
-        Node.by_level(root_right, 0, right_d)
-        left_m = max(left_d.items(), key=lambda item: len(item[1]))[0]
-        right_m = max(right_d.items(), key=lambda item: len(item[1]))[0]
-        return Node.ans(root_left, left_m, left_d) + Node.ans(
-            root_right, right_m, right_d
-        )
+        return self.solve(input_data, SwapMode.SUBTREE)
 
     @ec_samples(
         (
@@ -247,7 +179,7 @@ class Solution(SolutionBase[Output1, Output2, Output3]):
             ("part_1", TEST2, "EVERYBODYCODES"),
             ("part_2", TEST3, "MGFLNK"),
             ("part_3", TEST4, "DJMGL"),
-            ("part_3", TEST5, "DJCGL"),
+            ("part_3", TEST4 + "SWAP 5", "DJCGL"),
         )
     )
     def samples(self) -> None:
