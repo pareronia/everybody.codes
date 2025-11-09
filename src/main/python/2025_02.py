a73cc9f01d83fd62fb51db2239ba120bab53adec
pareronia <49491686+pareronia@users.noreply.github.com>
2025-11-09 11:59:37 +0100

    2025 Quest 2 - cleanup

--- src/main/python/2025_02.py
+++ src/main/python/2025_02.py
@@ -3,12 +3,13 @@
 # everybody.codes 2025 Quest 2
 #
 
+import itertools
 import sys
+from collections.abc import Iterator
 
 from ec.common import InputData
 from ec.common import SolutionBase
 from ec.common import ec_samples
-from ec.common import log
 
 Output1 = str
 Output2 = int
@@ -23,61 +24,44 @@ A=[35300,-64910]
 
 
 class Solution(SolutionBase[Output1, Output2, Output3]):
+    def calculate(
+        self, x: int, y: int, iterations: int, factor: int
+    ) -> Iterator[tuple[int, int]]:
+        a, b = 0, 0
+        for _ in range(iterations):
+            a, b = (
+                x + int((a * a - b * b) / factor),
+                y + int((2 * a * b) / factor),
+            )
+            yield a, b
+
+    def count(self, points: Iterator[tuple[int, int]]) -> int:
+        return sum(
+            all(
+                abs(a) <= 1_000_000 and abs(b) <= 1_000_000
+                for a, b in self.calculate(
+                    x, y, iterations=100, factor=100_000
+                )
+            )
+            for x, y in points
+        )
+
     def part_1(self, input_data: InputData) -> Output1:
         x, y = map(int, input_data[0][:-1][3:].split(","))
-        a, b = 0, 0
-        for _ in range(3):
-            a, b = a * a - b * b, a * b + b * a
-            a, b = a // 10, b // 10
-            a, b = a + x, b + y
+        a, b = list(self.calculate(x, y, iterations=3, factor=10))[-1]
         return f"[{a},{b}]"
 
     def part_2(self, input_data: InputData) -> Output2:
         x, y = map(int, input_data[0][:-1][3:].split(","))
-        ans = 0
-        cnt = 0
-        for yy in range(y, y + 1010, 10):
-            for xx in range(x, x + 1010, 10):
-                a, b = 0, 0
-                cnt += 1
-                for _ in range(100):
-                    a, b = a * a - b * b, a * b + b * a
-                    a = a // 100_000 if a > 0 else -((-a) // 100_000)
-                    b = b // 100_000 if b > 0 else -((-b) // 100_000)
-                    a, b = a + xx, b + yy
-                    if not (
-                        -1_000_000 <= a <= 1_000_000
-                        and -1_000_000 <= b <= 1_000_000
-                    ):
-                        break
-                else:
-                    ans += 1
-        log(cnt)
-        return ans
+        return self.count(
+            itertools.product(range(x, x + 1010, 10), range(y, y + 1010, 10))
+        )
 
     def part_3(self, input_data: InputData) -> Output3:
         x, y = map(int, input_data[0][:-1][3:].split(","))
-        log((x, y))
-        ans = 0
-        cnt = 0
-        for yy in range(y, y + 1001, 1):
-            for xx in range(x, x + 1001, 1):
-                a, b = 0, 0
-                cnt += 1
-                for _ in range(100):
-                    a, b = a * a - b * b, a * b + b * a
-                    a = a // 100_000 if a > 0 else -((-a) // 100_000)
-                    b = b // 100_000 if b > 0 else -((-b) // 100_000)
-                    a, b = a + xx, b + yy
-                    if not (
-                        -1_000_000 <= a <= 1_000_000
-                        and -1_000_000 <= b <= 1_000_000
-                    ):
-                        break
-                else:
-                    ans += 1
-        log(cnt)
-        return ans
+        return self.count(
+            itertools.product(range(x, x + 1001), range(y, y + 1001))
+        )
 
     @ec_samples(
         (
