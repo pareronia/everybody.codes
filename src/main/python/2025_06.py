2f91c248d8b32f35505ed42eb3c65af372375239
pareronia <49491686+pareronia@users.noreply.github.com>
2025-11-11 04:13:02 +0100

    2025 Quest 6 - faster

--- src/main/python/2025_06.py
+++ src/main/python/2025_06.py
@@ -8,19 +8,18 @@ from collections import Counter
 
 from ec.common import InputData
 from ec.common import SolutionBase
+from ec.common import ec_samples
 
 Output1 = int
 Output2 = int
 Output3 = int
 
-TEST1 = "ABabACacBCbca"
-TEST2 = "AABCBABCABCabcabcABCCBAACBCa"
+TEST = "ABabACacBCbca"
 
 
 class Solution(SolutionBase[Output1, Output2, Output3]):
     def count(self, input_data: InputData) -> Counter[str]:
-        ans = Counter[str]()
-        cnt = Counter[str]()
+        ans, cnt = Counter[str](), Counter[str]()
         for ch in input_data[0]:
             if ch.isupper():
                 cnt.update(ch.lower())
@@ -34,29 +33,39 @@ class Solution(SolutionBase[Output1, Output2, Output3]):
     def part_2(self, input_data: InputData) -> Output2:
         return sum(self.count(input_data).values())
 
-    def solve_3(
-        self, input_data: InputData, repeat: int = 1000, limit: int = 1000
-    ) -> int:
-        ans = 0
-        s = "".join(input_data[0] for _ in range(repeat))
-        for i, ch in enumerate(s):
-            if ch.isupper():
-                continue
-            lo, hi = max(0, i - limit), min(i + limit, len(s) - 1)
-            ans += s[lo : hi + 1].count(ch.upper())
-        return ans
-
     def part_3(self, input_data: InputData) -> Output3:
-        return self.solve_3(input_data)
+        limit = 1000
+        cnt_first, cnt_mid, cnt_last = (Counter[str]() for _ in range(3))
+        s = input_data[0]
+        size = len(s)
+        s3 = s * 3
+        for i, ch in enumerate(s):
+            if ch.islower():
+                lo, hi = size + i - limit, size + i + limit
+                for (start, end), cnt in zip(
+                    (
+                        (max(lo, size), hi),
+                        (lo, hi),
+                        (lo, min(hi, 2 * size - 1)),
+                    ),
+                    (cnt_first, cnt_mid, cnt_last),
+                    strict=True,
+                ):
+                    cnt[ch] += s3[start : end + 1].count(ch.upper())
+        return (
+            sum(cnt_first.values())
+            + 998 * sum(cnt_mid.values())
+            + sum(cnt_last.values())
+        )
 
+    @ec_samples(
+        (
+            ("part_1", TEST, 5),
+            ("part_2", TEST, 11),
+        )
+    )
     def samples(self) -> None:
-        input_data = tuple(TEST1.splitlines())
-        assert self.part_1(input_data) == 5
-        assert self.part_2(input_data) == 11
-        input_data = tuple(TEST2.splitlines())
-        assert self.solve_3(input_data, 1, 10) == 34
-        assert self.solve_3(input_data, 2, 10) == 72
-        assert self.solve_3(input_data, 1000, 1000) == 3442321
+        pass
 
 
 solution = Solution(2025, 6)
