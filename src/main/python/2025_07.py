0a2d6bd6bf694c1d54dc36d5db21e198fc084e56
pareronia <49491686+pareronia@users.noreply.github.com>
2025-11-12 00:49:43 +0100

    2025 Quest 7

---
+++ src/main/python/2025_07.py
@@ -0,0 +1,167 @@
+#! /usr/bin/env python3
+#
+# everybody.codes 2025 Quest 7
+#
+
+import sys
+from collections import defaultdict
+
+from ec.common import InputData
+from ec.common import SolutionBase
+from ec.common import ec_samples
+
+TEST1 = """\
+Oronris,Urakris,Oroneth,Uraketh
+
+r > a,i,o
+i > p,w
+n > e,r
+o > n,m
+k > f,r
+a > k
+U > r
+e > t
+O > r
+t > h
+"""
+TEST2 = """\
+Xanverax,Khargyth,Nexzeth,Helther,Braerex,Tirgryph,Kharverax
+
+r > v,e,a,g,y
+a > e,v,x,r
+e > r,x,v,t
+h > a,e,v
+g > r,y
+y > p,t
+i > v,r
+K > h
+v > e
+B > r
+t > h
+N > e
+p > h
+H > e
+l > t
+z > e
+X > a
+n > v
+x > z
+T > i
+"""
+TEST3 = """\
+Xaryt
+
+X > a,o
+a > r,t
+r > y,e,a
+h > a,e,v
+t > h
+v > e
+y > p,t
+"""
+TEST4 = """\
+Khara,Xaryt,Noxer,Kharax
+
+r > v,e,a,g,y
+a > e,v,x,r,g
+e > r,x,v,t
+h > a,e,v
+g > r,y
+y > p,t
+i > v,r
+K > h
+v > e
+B > r
+t > h
+N > e
+p > h
+H > e
+l > t
+z > e
+X > a
+n > v
+x > z
+T > i
+"""
+
+Output1 = str
+Output2 = int
+Output3 = int
+
+
+class Solution(SolutionBase[Output1, Output2, Output3]):
+    def part_1(self, input_data: InputData) -> Output1:
+        names = input_data[0].split(",")
+        edges = defaultdict[str, set[str]](set)
+        for line in input_data[2:]:
+            a, b = line.split(" > ")
+            edges[a] |= set(b.split(","))
+
+        for name in names:
+            for j in range(1, len(name)):
+                if name[j] not in edges[name[j - 1]]:
+                    break
+            else:
+                return name
+        raise AssertionError
+
+    def part_2(self, input_data: InputData) -> Output2:
+        names = input_data[0].split(",")
+        edges = defaultdict[str, set[str]](set)
+        for line in input_data[2:]:
+            a, b = line.split(" > ")
+            edges[a] |= set(b.split(","))
+
+        ans = 0
+        for i, name in enumerate(names, start=1):
+            for j in range(1, len(name)):
+                if name[j] not in edges[name[j - 1]]:
+                    break
+            else:
+                ans += i
+        return ans
+
+    def part_3(self, input_data: InputData) -> Output3:
+        names = input_data[0].split(",")
+        edges = defaultdict[str, set[str]](set)
+        for line in input_data[2:]:
+            a, b = line.split(" > ")
+            edges[a] |= set(b.split(","))
+
+        def dfs(name: str, ans: set[str]) -> None:
+            if 7 <= len(name) <= 11:
+                ans.add(name)
+            if len(name) <= 10:
+                for ch in edges.get(name[-1], {}):
+                    dfs(name + ch, ans)
+
+        ans = set[str]()
+        for name in names:
+            for j in range(1, len(name)):
+                if name[j] not in edges[name[j - 1]]:
+                    break
+            else:
+                dfs(name, ans)
+        return len(ans)
+
+    @ec_samples(
+        (
+            ("part_1", TEST1, "Oroneth"),
+            ("part_2", TEST2, 23),
+            ("part_3", TEST3, 25),
+            ("part_3", TEST4, 1154),
+        )
+    )
+    def samples(self) -> None:
+        pass
+
+
+solution = Solution(2025, 7)
+
+
+def main() -> None:
+    solution.run(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
